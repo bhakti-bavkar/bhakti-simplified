fc76aa5501e183ee1c2c6f8b234c815f629f3bbe
bhakti-bavkar <bhakti.t88@gmail.com>
2017-03-21 14:11:41 -0700

    Add files via upload

---
+++ StackFriends/Node.h
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+class Node
+{
+	int data;
+	Node* next;
+
+public:
+	Node();
+	//Node(int data);
+	Node* getNode();
+	int getData();
+	Node* getNext();
+	void setNode(Node* N);
+	void setData(int v);
+	void setNext(Node* N);
+	friend void moveNext(Node* N);
+	//Node& operator = (const Node& node);
+	~Node();
+
+};
---
+++ StackFriends/Stack.h
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "Node.cpp"
+
+using namespace std;
+
+class Stack
+{
+	Node* top;
+	size_t size ;
+	int stack_count;
+
+	bool isEmpty();
+	bool isFull();
+
+public:
+	Stack();
+	Stack(size_t size);
+	void push(int data);
+	int pop();
+	void EmptyStack();
+	friend void moveNext(Node* N);
+
+};
